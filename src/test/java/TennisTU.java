ee7c61db7ff510b848dbca8c9baaa8bf53d690ca
MathiasIZZ <mathias.guiguen@gmail.com>
2022-07-19 14:50:34 +0200

    step2

--- src/test/java/TennisTU.java
+++ src/test/java/TennisTU.java
@@ -13,6 +13,38 @@ public class TennisTU {
     Partie laPartie = new Partie(JoueurA, joueurB);
 
 
+    @Test
+    @DisplayName("Un utilisateur peut créer une nouvelle partie.")
+    public void CreatePartie() {
+        assertInstanceOf(Partie.class, this.laPartie);
+    }
+
+    @Test
+    @DisplayName("Un utilisateur peut choisir les 2 joueurs qui jouerons la partie.")
+    public void createPartieWithPlayers() {
+        this.laPartie.setJoueurA(new Joueur("Mathias"));
+        this.laPartie.setJoueurB(new Joueur("Karan"));
+
+        assertInstanceOf(Partie.class, this.laPartie);
+    }
+
+    @Test
+    @DisplayName("Au début de la partie les deux joueurs ont 0 points.")
+    public void initializePartie() {
+
+//        this.laPartie.getJoueurA().setScore("0");
+//        this.laPartie.getJoueurB().setScore("0");
+
+        assertEquals("0", this.laPartie.getJoueurA().getScore());
+        assertEquals("0", this.laPartie.getJoueurB().getScore());
+    }
+
+
+
+
+
+
+
 
 
 
