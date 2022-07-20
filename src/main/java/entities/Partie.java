f0b591f2dc43fc471db529636a71a8375a8d6ab6
kalakaran.consalus <kalakaran.consalus@cgi.com>
2022-07-20 14:25:51 +0200

    Tests unitaires Fixed error with while

--- src/main/java/entities/Partie.java
+++ src/main/java/entities/Partie.java
@@ -30,61 +30,56 @@ public class Partie {
     }
 
     public void gagnePoint(Joueur joueurGagnant) {
-        if (joueurGagnant.getScore() == 0 && !joueurGagnant.isJeuDecisif()) {
-            joueurGagnant.setScore(15);
-        }
-        else if (joueurGagnant.getScore() == 15) {
-            joueurGagnant.setScore(30);
-        }
-        else if (joueurGagnant.getScore() == 30) {
-            joueurGagnant.setScore(40);
-        }
-        else {
-            Joueur joueurPerdant;
-            if (joueurGagnant.equals(joueurA)) {
-                joueurPerdant = joueurB;
-            } else {
-                joueurPerdant = joueurA;
+        if(!joueurGagnant.isWin()) {
+            if (joueurGagnant.getScore() == 0 && !joueurGagnant.isJeuDecisif()) {
+                joueurGagnant.setScore(15);
             }
-            if (!joueurGagnant.isAvantage() && !joueurPerdant.isAvantage()) {
-                joueurGagnant.setAvantage(true);
-            } else if (joueurPerdant.isAvantage()) {
-                joueurPerdant.setAvantage(false);
-            } else if (joueurGagnant.isAvantage()) {
-                joueurGagnant.setScore(0);
-                joueurPerdant.setScore(0);
-                joueurGagnant.setNombreJeu(joueurGagnant.getNombreJeu() + 1);
-
-                if (joueurGagnant.getNombreJeu() == 6 && joueurPerdant.getNombreJeu() <= 4) {
-                    joueurGagnant.setNombreSet(joueurGagnant.getNombreSet() + 1);
-                    if(joueurGagnant.getNombreSet() == 2) {
-                        joueurGagnant.setWin(true);
-                    }
-                }
-                else if (joueurGagnant.getNombreJeu() == 7 && joueurPerdant.getNombreJeu() == 5) {
-
-                    joueurGagnant.setNombreSet(joueurGagnant.getNombreSet() + 1);
-                    if(joueurGagnant.getNombreSet() == 2) {
-                        joueurGagnant.setWin(true);
-                    }
-                }
-                else if (joueurGagnant.getNombreJeu() == joueurPerdant.getNombreJeu()) {
-                    joueurGagnant.setJeuDecisif(true);
-                    joueurPerdant.setJeuDecisif(true);
-                }
+            else if (joueurGagnant.getScore() == 15) {
+                joueurGagnant.setScore(30);
             }
-            if(joueurGagnant.isJeuDecisif() && joueurPerdant.isJeuDecisif()) {
-                if (joueurGagnant.getScore() < 7) {
-
-                    joueurGagnant.setScore(joueurGagnant.getScore() + 1);
-
-                    if ((joueurGagnant.getScore() - joueurPerdant.getScore()) > 2) {
-
+            else if (joueurGagnant.getScore() == 30) {
+                joueurGagnant.setScore(40);
+            }
+            else {
+                Joueur joueurPerdant;
+                if (joueurGagnant.equals(joueurA)) {
+                    joueurPerdant = joueurB;
+                } else {
+                    joueurPerdant = joueurA;
+                }
+                if (!joueurGagnant.isAvantage() && !joueurPerdant.isAvantage()) {
+                    joueurGagnant.setAvantage(true);
+                } else if (joueurPerdant.isAvantage()) {
+                    joueurPerdant.setAvantage(false);
+                } else if (joueurGagnant.isAvantage()) {
+                    joueurGagnant.setScore(0);
+                    joueurPerdant.setScore(0);
+                    joueurGagnant.setNombreJeu(joueurGagnant.getNombreJeu() + 1);
+                    if (joueurGagnant.getNombreJeu() == 6 && joueurPerdant.getNombreJeu() <= 4) {
                         joueurGagnant.setNombreSet(joueurGagnant.getNombreSet() + 1);
-
                         if(joueurGagnant.getNombreSet() == 2) {
-
                             joueurGagnant.setWin(true);
+                        }
+                    }
+                    else if (joueurGagnant.getNombreJeu() == 7 && joueurPerdant.getNombreJeu() == 5) {
+                        joueurGagnant.setNombreSet(joueurGagnant.getNombreSet() + 1);
+                        if(joueurGagnant.getNombreSet() == 2) {
+                            joueurGagnant.setWin(true);
+                        }
+                    }
+                    else if (joueurGagnant.getNombreJeu() == joueurPerdant.getNombreJeu()) {
+                        joueurGagnant.setJeuDecisif(true);
+                        joueurPerdant.setJeuDecisif(true);
+                    }
+                }
+                if(joueurGagnant.isJeuDecisif() && joueurPerdant.isJeuDecisif()) {
+                    if (joueurGagnant.getScore() < 7) {
+                        joueurGagnant.setScore(joueurGagnant.getScore() + 1);
+                        if ((joueurGagnant.getScore() - joueurPerdant.getScore()) > 2) {
+                            joueurGagnant.setNombreSet(joueurGagnant.getNombreSet() + 1);
+                            if(joueurGagnant.getNombreSet() == 2) {
+                                joueurGagnant.setWin(true);
+                            }
                         }
                     }
                 }
